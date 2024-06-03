9be0b18d1c2803f0845e11eabd7fc82a1b5d3d40
overthemoon11 <shu.hui051603@gmail.com>
2024-06-03 14:45:54 +0800

    change branch upload

--- frontend/pages/login.php
+++ frontend/pages/login.php
@@ -3,12 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="../assets/styles/login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
+    <?php include '../components/header.php'; ?>
     <main>
-        <?php include '../components/header.php'; ?>
-        <h1>HI login</h1>
+        <div class="login-container">
+            <h1>Login</h1>
+            <p>Doesn’t have an account yet? <a href="register.php">Sign Up</a></p>
+            <form class="login-form" action="login.php" method="POST">
+                <label for="email">Email address</label>
+                <input type="email" id="email" name="email" required>
+                
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+                
+                <a href="resetpassword.php" class="forgot-password">Forgot password?</a>
+                
+                <button type="submit">LOGIN</button>
+            </form>
+        </div>
     </main>
     <?php include '../components/footer.php'; ?>
 </body>
